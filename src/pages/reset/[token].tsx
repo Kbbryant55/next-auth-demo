b6af9c38cca8b850aae1f6e87ef82718a8456f6d
Kbbryant55 <kwilloughby55@gmail.com>
2023-03-08 23:48:43 -0800

    final commit

--- src/pages/reset/[token].tsx
+++ src/pages/reset/[token].tsx
@@ -10,7 +10,7 @@ const Reset = ({ token }: { token: string }) => {
         <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex items-center justify-center flex-col">
           <ResetForm token={token} />
         </div>
-        {/*----Background----*/}
+        {/*----Background here----*/}
         <Background image={"../../auth/reset.jpg"} />
       </div>
     </div>
